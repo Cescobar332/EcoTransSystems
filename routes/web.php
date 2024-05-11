8bc4f0d5e8c15f89ed5b6150fd468f8e3e7a776f
Cescobar332 <cescobar332@unab.edu.co>
2024-05-11 18:48:08 -0500

    Borrado de migraciones y crud

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SesionController;
-use App\Http\Controllers\UsuarioController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/registro', [UsuarioController::class, 'formularioRegistro'])->name('registro');
-Route::post('/registro', [UsuarioController::class, 'guardarUsuario']);
-Route::get('/login', [SesionController::class, 'formularioInicioSesion'])->name('login');
-Route::post('/login', [SesionController::class, 'inicioSesion'])->name('login.post');
-// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-// Route::post('/dashboard/action', [DashboardController::class, 'store'])->name('dashboard.store');
-// Route::get('/recuperar-contrasena', [SesionController::class, 'recuperarContrasena'])->name('recuperar_contrasena');
-// Route::post('/recuperar-contrasena', [SesionController::class, 'recContra'])->name('recuperar_contrasena.post');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('usuarios', UsuarioController::class);
 
-//Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
