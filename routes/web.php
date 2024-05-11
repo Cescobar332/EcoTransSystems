a2f56a21f69ec5e56775aef4b83589dad7064161
Inge Fabian Suarez <fsuarez120@unab.edu.co>
2024-05-11 18:12:24 -0500

    Se creo el login el register y todo lo que tiene que ver con la autenticacion

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,15 @@ use App\Http\Controllers\Auth\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
-// Ruta para mostrar el formulario de registro
+/* // Ruta para mostrar el formulario de registro
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
-
-// Ruta para manejar el proceso de registro
+ */
+/* // Ruta para manejar el proceso de registro
 Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login']); */
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
