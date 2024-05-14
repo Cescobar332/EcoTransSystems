35af3b16a65e7674522a5ccd173168d764d3b23a
Cescobar332 <cescobar332@unab.edu.co>
2024-05-14 00:39:45 -0500

    DatabaseSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -18,7 +18,7 @@ class RoleSeeder extends Seeder
         $role1 = Role::create(['name' => 'Admin']);
         $role2 = Role::create(['name' => 'User']);
 
-        Permission::create(['name' => 'admin.estadisticas'])->assignRole($role1);
+        Permission::create(['name' => 'estadisticas'])->assignRole($role1);
 
     }
 }
